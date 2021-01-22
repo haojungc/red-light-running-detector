04a490d8cc33bfb80b5e47d88c1bfa68f69f089b
Nick Chen <nickchenchj@gmail.com>
2021-01-22 19:42:45 +0800

    Migrate to Python 3

--- darknet/python/darknet.py
+++ darknet/python/darknet.py
@@ -148,10 +148,10 @@ if __name__ == "__main__":
     #im = load_image("data/wolf.jpg", 0, 0)
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
-    #print r[:10]
+    #print(r[:10])
     net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
     meta = load_meta("cfg/coco.data")
     r = detect(net, meta, "data/dog.jpg")
-    print r
+    print(r)
     
 
